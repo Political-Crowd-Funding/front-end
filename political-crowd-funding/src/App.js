a7729663e175db36694a59ef8f1c34950cd535da
Mario Amaya <thefozcat@gmail.com>
2019-10-25 10:13:34 -0700

    initial Auth0 configuration

--- political-crowd-funding/src/App.js
+++ political-crowd-funding/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import NavBar from './components/NavBar';
+import { useAuth0 } from './components/Auth0/Auth0';
 
 import './App.css';
 
 function App() {
+
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return (
+      <div>Loading...</div> // replace with loader-spinner
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar/>
         <h1>Political Crowd Funding</h1>
       </header>
     </div>
--- political-crowd-funding/src/index.js
+++ political-crowd-funding/src/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from './components/Auth0/Auth0';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const onRedirectCallback = appState => {
+    window.history.replaceState(
+      {},
+      document.title,
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+};
 
-
-
-
-
+ReactDOM.render(
+    <Auth0Provider
+        domain={process.env.REACT_APP_DOMAIN}
+        client_id={process.env.REACT_APP_CLIENT_ID}
+        redirect_uri={window.location.origin}
+        onRedirectCallback={onRedirectCallback}
+    >
+        <App />
+    </Auth0Provider>,
+    document.getElementById('root')
+);
